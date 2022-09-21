f6bdab5ef5acb754ccdafbc9ab01513315a312ce
jonx1183 <jonx1183@stud.kea.dk>
2022-09-21 10:19:51 +0200

    Initial commit

---
+++ src/main/java/com/example/madspild/MadSpildApplication.java
@@ -0,0 +1,13 @@
+package com.example.madspild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadSpildApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MadSpildApplication.class, args);
+  }
+
+}
